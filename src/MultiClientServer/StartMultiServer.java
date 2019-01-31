5d5cc8dc08a36b66fc394638fe168feb4ed1ddef
Momcilo <s315282@stud.hioa.no>
2019-01-31 11:27:02 +0100

    Fixing

--- src/MultiClientServer/StartMultiServer.java
+++ src/MultiClientServer/StartMultiServer.java
@@ -4,5 +4,6 @@ public class StartMultiServer {
     public static void main(String[] args) throws Exception {
         ServerTask serverTask = new ServerTask(5555);
         serverTask.start();
+        System.out.println("Server is running, please call the Clients.");
     }
 }
